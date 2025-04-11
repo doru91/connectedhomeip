47a181837bffb52e80fd787fc76a9d41f7f3568b
Doru Gucea <doru-cristian.gucea@nxp.com>
2025-04-11 14:47:28 +0300

    [E2E][JF] jf-control-app: Stubs for the two JF Clusters
    Signed-off-by: Doru Gucea <doru-cristian.gucea@nxp.com>

--- src/app/server/Server.h
+++ src/app/server/Server.h
@@ -33,6 +33,7 @@
 #include <app/server/AppDelegate.h>
 #include <app/server/CommissioningWindowManager.h>
 #include <app/server/DefaultAclStorage.h>
+#include <app/server/JointFabricDatastore.h>
 #include <credentials/CertificateValidityPolicy.h>
 #include <credentials/FabricTable.h>
 #include <credentials/GroupDataProvider.h>
@@ -415,6 +416,8 @@ public:
     app::DefaultSafeAttributePersistenceProvider & GetDefaultAttributePersister() { return mAttributePersister; }
 
     app::reporting::ReportScheduler * GetReportScheduler() { return mReportScheduler; }
+    
+    app::JointFabricDatastore & GetJointFabricDatastore() { return mJointFabricDatastore; }
 
 #if CHIP_CONFIG_ENABLE_ICD_SERVER
     app::ICDManager & GetICDManager() { return mICDManager; }
@@ -694,6 +697,8 @@ private:
 
     Access::AccessControl mAccessControl;
     app::AclStorage * mAclStorage;
+    
+    app::JointFabricDatastore mJointFabricDatastore;
 
     TestEventTriggerDelegate * mTestEventTriggerDelegate;
     Crypto::OperationalKeystore * mOperationalKeystore;
